a0cddc1a01359cf933aee02d5eedff5114c7dc96
xia-hao <18188907492@163.com>
2020-05-09 08:45:31 +0800

    全部上传

--- src/components/homemain.vue
+++ src/components/homemain.vue
@@ -14,6 +14,7 @@
             type="primary"
             icon="el-icon-search"
             style="width: 80px;height: 30px"
+            :disabled="productname== ''"
             @click="search"
           >搜索</el-button>
         </el-form-item>
@@ -68,7 +69,7 @@
     name: "homemain",
     data(){
       return{
-        productname: undefined,
+        productname: '',
         categorylist: undefined
       }
     },
